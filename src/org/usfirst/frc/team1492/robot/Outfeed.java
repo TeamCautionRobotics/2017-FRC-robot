8bc102fc477ac82ca72ec8d1d46e747765af41be
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2017-09-30 15:26:29 -0700

    Clean up formatting in GearPiston.java, HumanLoadLight.java, Outfeed.java, and Winch.java

--- src/org/usfirst/frc/team1492/robot/Outfeed.java
+++ src/org/usfirst/frc/team1492/robot/Outfeed.java
@@ -3,14 +3,14 @@ package org.usfirst.frc.team1492.robot;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class Outfeed {
-	private VictorSP outfeed;
+    private VictorSP outfeed;
 
-	public Outfeed(int motorChannel) {
-		outfeed = new VictorSP(motorChannel);
-	}
-	
-	public void moveOutfeed(double motorPower) {
-		outfeed.set(motorPower);
-	}
+    public Outfeed(int motorChannel) {
+        outfeed = new VictorSP(motorChannel);
+    }
+
+    public void moveOutfeed(double motorPower) {
+        outfeed.set(motorPower);
+    }
 
 }
