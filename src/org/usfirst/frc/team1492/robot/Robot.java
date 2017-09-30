afe2598bc4f2706d445af1c711dd43008b5406c9
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2017-09-30 15:15:59 -0700

    Remove unused code and comments in Robot.java

--- src/org/usfirst/frc/team1492/robot/Robot.java
+++ src/org/usfirst/frc/team1492/robot/Robot.java
@@ -70,7 +70,6 @@ public class Robot extends IterativeRobot {
 
         gearPiston = new GearPiston(1);
 
-        // TODO: Limit switch for winch
         winch = new Winch(3);
 
         doors = new Doors(2, 3, 4);
@@ -95,27 +94,11 @@ public class Robot extends IterativeRobot {
         crossBaseline.add(commandFactory.moveStraightDistance(true, 0.4, 65, false));
         crossBaseline.add(commandFactory.moveStraight(true, -0.05, 0.1, true));
         missionChooser.addObject("cross baseline mission", crossBaseline);
-//
-//        Mission missionLeftCamera = new Mission(14);
-//        missionLeftCamera.add(commandFactory.moveStraightDistance(true, 0.4, 60, false));
-//        missionLeftCamera.add(commandFactory.moveStraight(true, -0.05, 0.1, true));
-//        missionLeftCamera.add(commandFactory.turnInPlace(false, 0.4, -45));
-//        missionLeftCamera.add(commandFactory.turnToTarget());
-//        missionLeftCamera.add(commandFactory.moveStraightDistance(true, 0.4, 37.5, false));
-//        missionLeftCamera.add(commandFactory.moveStraight(true, -0.05, 0.1, true));
-//        missionLeftCamera.add(commandFactory.delay(0.6));
-//        missionLeftCamera.add(commandFactory.setGearPiston(true));
-//        missionLeftCamera.add(commandFactory.delay(0.8));
-//        missionLeftCamera.add(commandFactory.moveStraight(false, -0.4, 0.2));
-//        missionLeftCamera.add(commandFactory.setGearPiston(false));
-//        missionChooser.addObject("mission left camera", missionLeftCamera);
 
         Mission missionCenterGearNoCamera = new Mission("center gear encoder");
         missionCenterGearNoCamera.add(commandFactory.moveStraightDistance(true, 0.4, 65, false));
         missionCenterGearNoCamera.add(commandFactory.moveStraight(true, -0.05, 0.1, true));
-//        missionCenter.add(commandFactory.moveStraight(true, 0.2, 0.2, true));
         missionCenterGearNoCamera.add(commandFactory.delay(0.4));
-//        missionCenter.add(commandFactory.alignWithVision());
         missionCenterGearNoCamera.add(commandFactory.setGearPiston(true));
         missionCenterGearNoCamera.add(commandFactory.delay(0.8)); 
         missionCenterGearNoCamera.add(commandFactory.moveStraight(false, -0.4, 0.2));
@@ -157,12 +140,6 @@ public class Robot extends IterativeRobot {
         missionRightNoCameraNoGear.add(commandFactory.moveStraight(true, -0.05, 0.1, true));
         missionRightNoCameraNoGear.add(commandFactory.moveStraight(false, 0, 0));
         missionRightNoCameraNoGear.add(commandFactory.delay(1));
-//        missionRightCameraNoGear.add(commandFactory.moveStraightDistance(true, 0.4, 15, true));
-//        missionRightCameraNoGear.add(commandFactory.delay(0.6));
-//        missionRightCameraNoGear.add(commandFactory.setGearPiston(true));
-//        missionRightCameraNoGear.add(commandFactory.delay(0.8));
-//        missionRightCameraNoGear.add(commandFactory.moveStraight(false, -0.4, 0.4));
-//        missionRightCameraNoGear.add(commandFactory.setGearPiston(false));
         missionChooser.addObject("mission right no camera, no gear", missionRightNoCameraNoGear);
         
         Mission missionleftCameraGear = new Mission("left gear camera");
@@ -188,12 +165,6 @@ public class Robot extends IterativeRobot {
         missionLeftNoCameraNoGear.add(commandFactory.moveStraight(true, -0.05, 0.1, true));
         missionLeftNoCameraNoGear.add(commandFactory.moveStraight(false, 0, 0));
         missionLeftNoCameraNoGear.add(commandFactory.delay(1));
-//        missionLeftCameraNoGear.add(commandFactory.moveStraightDistance(true, 0.4, 15, true));
-//        missionLeftCameraNoGear.add(commandFactory.delay(0.6));
-//        missionLeftCameraNoGear.add(commandFactory.setGearPiston(true));
-//        missionLeftCameraNoGear.add(commandFactory.delay(0.8));
-//        missionLeftCameraNoGear.add(commandFactory.moveStraight(false, -0.4, 0.4));
-//        missionLeftCameraNoGear.add(commandFactory.setGearPiston(false));
         missionChooser.addObject("mission left no camera, no gear", missionLeftNoCameraNoGear);
         
         Mission doNothing = new Mission("do nothing");
@@ -207,10 +178,6 @@ public class Robot extends IterativeRobot {
         commandFactory.moveStraight(false, 0, 0),
         commandFactory.alignWithVision(),
         commandFactory.moveStraight(false, 0.4, 0.5),
-//        commandFactory.setGearPiston(true),
-//        commandFactory.delay(0.8),
-//        commandFactory.moveStraight(false, -0.4, 1.0),
-//        commandFactory.setGearPiston(false),
         commandFactory.moveStraight(true, 0, 0));
         missionChooser.addObject("deploy gear", deployGear);
 
